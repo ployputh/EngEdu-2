8a43789859ceaaf270436944678225a1d328ebce
Sirada Soonthorn <ploy_puth@outlook.co.th>
2019-10-21 19:25:49 +0700

    “first year semester 2”

--- EngEdu-2/EngEdu-2/Program.cs
+++ EngEdu-2/EngEdu-2/Program.cs
@@ -11,6 +11,7 @@ namespace EngEdu_2
         static void Main(string[] args)
         {
             Console.WriteLine("Year 225x semester 1,I am an Engedu freshman.");
+            Console.WriteLine("Year 255x semster 2, I learn to program in C sharp.");
         }
     }
 }
